57a43b856ed4ac3c4fdd383c2d572894eada9e23
mirzobox <mirzoulugbekkhudoyberdiyev@gmail.com>
2024-06-12 12:57:47 +0500

    Setup initial structure of login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,24 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import ProtectedRoutes from "./components/ProtectedRoutes";
+import RootLayouts from "./layouts/RootLayouts";
+import Login from "./page/Login";
+
 export default function App() {
-  return <div>Deploying...</div>;
+  const user = null;
+  const routes = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <ProtectedRoutes user={user}>
+          <RootLayouts />
+        </ProtectedRoutes>
+      ),
+      children: [],
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+  ]);
+  return <RouterProvider router={routes} />;
 }
