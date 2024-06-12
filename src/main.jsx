94395da0733bca479c28d5fbbfda03db246dace9
mirzobox <mirzoulugbekkhudoyberdiyev@gmail.com>
2024-06-12 15:30:10 +0500

    Add sonner

--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import React from "react";
+import { Provider } from "react-redux";
+import { store } from "./redux/store/index.js";
+import { Toaster } from "sonner";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <>
+    <Provider store={store}>
+      <App />
+    </Provider>
+    <Toaster position="top-center" />
+  </>,
+);
