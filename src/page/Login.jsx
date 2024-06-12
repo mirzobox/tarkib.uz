afd739216713673674368114bc1fa35611970c64
mirzobox <mirzoulugbekkhudoyberdiyev@gmail.com>
2024-06-12 13:51:36 +0500

    Reinstallation

--- src/page/Login.jsx
+++ src/page/Login.jsx
@@ -1,3 +1,17 @@
+import { Spinner } from "@material-tailwind/react";
+
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <section className="flex h-full w-full items-center justify-center bg-white px-5">
+      <div className="w-full max-w-96 rounded-md p-5 shadow-md">
+        <h1 className="mb-5 text-center text-xl font-bold">Kirish</h1>
+        <div className="flex w-full flex-col">
+          <div className="flex gap-5">
+            <Spinner />
+            Loading...
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
